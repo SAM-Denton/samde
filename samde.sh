95e1782db50b4559de5f9c4c8021e7920324ba05
Sam Denton <12024713-SamDenton@users.noreply.gitlab.com>
2022-10-24 22:15:56 -0400

    added --depth to git clones and modified pacman commands

--- samde.sh
+++ samde.sh
@@ -9,21 +9,22 @@ sudo sed -i "93,94s/#//g;33s/#//g;37s/#//g" /etc/pacman.conf
 sudo sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 sudo sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 
-sudo pacman --noconfirm --needed -Syu - < core.txt || error "Failed to install packages"
-sudo pacman --noconfirm --needed -Syu - < pkgs.txt || error "Failed to install packages"
+sudo pacman -Syu --noconfirm
+sudo pacman --noconfirm --needed -S - < core.txt || error "Failed to install packages"
+sudo pacman --noconfirm --needed -S - < pkgs.txt || error "Failed to install packages"
 
 sudo usermod -s /bin/zsh $USER
 echo "export ZDOTDIR=\"\$HOME\"/.config/zsh" | sudo tee -a /etc/zsh/zshenv
 touch $HOME/.cache/zsh/history
 
-git clone --separate-git-dir=$HOME/.config/dots https://gitlab.com/samdenton/dots.git tmpdots
+git clone --depth 1 --separate-git-dir=$HOME/.config/dots https://gitlab.com/samdenton/dots.git tmpdots
 rsync --recursive --verbose --exclude '.git' tmpdots/ $HOME/
 rm -rf tmpdots
 /usr/bin/git --git-dir=$HOME/.config/dots --work-tree=$HOME config --local status.showUntrackedFiles no
 
 for i in "${utils[@]}"
 do 
-    git clone https://gitlab.com/samdenton/$i.git $HOME/.local/src/$i
+    git clone --depth 1 https://gitlab.com/samdenton/$i.git $HOME/.local/src/$i
     sudo make clean install -C $HOME/.local/src/$i
 done
 
