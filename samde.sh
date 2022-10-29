4eec43fc11101ff27ec511de772b1041a8729e55
Sam Denton <12024713-SamDenton@users.noreply.gitlab.com>
2022-10-28 21:43:14 -0400

    removed core.txt and added comments

--- samde.sh
+++ samde.sh
@@ -3,37 +3,43 @@
 utils=(dwm dmenu st slstatus dnote)
 folders=(.local .config .cache .cache/zsh)
 
+# Checking for folders and creating them if they're absent
 for i in "${folders[@]}"; do [ -d $HOME/$i ] || mkdir $HOME/$i; done
 
+# Enabling multilib, ILoveCandy and adding the number of cpu threads in makepkg.conf
 sudo sed -i "93,94s/#//g;33s/#//g;37s/#//g" /etc/pacman.conf
 sudo sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 sudo sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 
+# Installing programs in pkg.txt
 sudo pacman -Syu --noconfirm
-sudo pacman --noconfirm --needed -S - < core.txt || error "Failed to install packages"
 sudo pacman --noconfirm --needed -S - < pkgs.txt || error "Failed to install packages"
 
+# Making zsh the default shell
 sudo usermod -s /bin/zsh $USER
 echo "export ZDOTDIR=\"\$HOME\"/.config/zsh" | sudo tee -a /etc/zsh/zshenv
 touch $HOME/.cache/zsh/history
 
+# Cloning dotfiles and putting them in .config
 git clone --depth 1 --separate-git-dir=$HOME/.config/dots https://gitlab.com/samdenton/dots.git tmpdots
 rsync --recursive --verbose --exclude '.git' tmpdots/ $HOME/
 rm -rf tmpdots
 /usr/bin/git --git-dir=$HOME/.config/dots --work-tree=$HOME config --local status.showUntrackedFiles no
 
+# Cloning my suckless utilities and compiling
 for i in "${utils[@]}"
 do 
     git clone --depth 1 https://gitlab.com/samdenton/$i.git $HOME/.local/src/$i
     sudo make clean install -C $HOME/.local/src/$i
 done
 
-
+# Installing the yay aur helper
 git clone https://aur.archlinux.org/yay-bin
 cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
 yay -S lf-bin papirus-folders-git
 sudo papirus-folders -C indigo
 
+# Installing vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim -c "PlugInstall|q|q"
